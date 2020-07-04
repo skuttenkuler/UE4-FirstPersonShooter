ae7ce7a1b1bf6e027030ebbf0abf142859492a9f
skuttenkuler <sam.kuttenk@gmail.com>
2020-07-04 13:49:02 -0700

    3rd person camera and spring arm class set

---
+++ Source/Shooter/Camera.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "Camera.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API UCamera : public UCameraComponent
+{
+	GENERATED_BODY()
+	
+};
