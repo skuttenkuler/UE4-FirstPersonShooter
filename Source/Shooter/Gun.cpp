fb153597872159b0397df5210e978a15ca6dfea4
skuttenkuler <sam.kuttenk@gmail.com>
2020-07-05 20:06:35 -0700

    debug point, line tracing displaying with every click inside max range

--- Source/Shooter/Gun.cpp
+++ Source/Shooter/Gun.cpp
@@ -35,7 +35,18 @@ void AGun::PullTrigger()
     FRotator Rotation;
     OwnerController->GetPlayerViewPoint(Location, Rotation);
     
-   DrawDebugCamera(GetWorld(), Location, Rotation, 90, 2, FColor::Red, true);
+    //bullet endpoint
+    FVector End = Location + Rotation.Vector() * MaxRange;
+    
+    
+    //LineTrace
+    FHitResult Hit;
+    bool bSuccess = GetWorld()->LineTraceSingleByChannel(Hit, Location, End, ECollisionChannel::ECC_GameTraceChannel1);
+    if(bSuccess)
+    {
+        DrawDebugPoint(GetWorld(), Hit.Location, 20, FColor::Red, true);
+    }
+    //DrawDebugCamera(GetWorld(), Location, Rotation, 90, 2, FColor::Red, true);
 }
 
 // Called when the game starts or when spawned
